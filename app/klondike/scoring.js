1a010e5ba647fab91a4d2137b083983ec62d1a8a
jens <jho@orgcomnet.de>
2015-12-15 23:34:01 +0100

    * added es6 classes

--- app/klondike/scoring.js
+++ app/klondike/scoring.js
@@ -1,30 +1,35 @@
-export function Scoring() {
-    this.score = 0;
+function scoreForMoving(source, destionation) {
+    if(destionation.name === "TableauPile") {
+        if(source.name === "FoundationPile") {
+            return -15;
+        }
+        return 5;
+    }
+    if(destionation.name === "FoundationPile") {
+        if(source.name === "TableauPile" || source.name === "WastePile") {
+            return 10;
+        }
+    }
+}
 
-    this.newGame = function () {
+export class Scoring {
+    constructor() {
         this.score = 0;
-    };
-    this.tableauCardTurnedUp = function () {
-        this.score += 5;
-    };
-    this.dropped = function (source, destionation) {
-        this.score += scoreForMoving(source, destionation) || 0;
-    };
-    this.wasteRecycled = function () {
-        this.score = Math.max(this.score - 100, 0);
-    };
+    }
 
-    function scoreForMoving(source, destionation) {
-        if(destionation.name === "TableauPile") {
-            if(source.name === "FoundationPile") {
-                return -15;
-            }
-            return 5;
-        }
-        if(destionation.name === "FoundationPile") {
-            if(source.name === "TableauPile" || source.name === "WastePile") {
-                return 10;
-            }
-        }
+    newGame() {
+        this.score = 0;
+    }
+
+    tableauCardTurnedUp() {
+        this.score += 5;
+    }
+
+    dropped(source, destionation) {
+        this.score += scoreForMoving(source, destionation) || 0;
+    }
+
+    wasteRecycled() {
+        this.score = Math.max(this.score - 100, 0);
     }
 }
