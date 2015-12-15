8cc0987908533c1162ddd8841eebafa4d9b5a5c5
jens <jho@orgcomnet.de>
2015-12-15 23:20:27 +0100

    * added the klondike dependencies.

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import "klondike/scoring.js";
 import "klondike/klondike.js";
 import "klondike/board.js";
 import "klondike/game.js";
-
+//
 angular.module("solitaire", ["klondike", "ngDraggable"]);
 
 angular.element(document).ready(() => {
