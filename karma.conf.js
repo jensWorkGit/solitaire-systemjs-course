405c865992f3069354b61d362db272ab80a778e8
jhoffmann <jens.hoffmann@lidl.com>
2015-12-18 10:29:04 +0100

    * added karma to the karma.conf.js but still has some errors.

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,55 @@
 /* eslint-env node*/
 
 module.exports = function (config) {
-  "use strict";
+    "use strict";
 
-  config.set({
+    config.set({
 
-    basePath: "./",
+        basePath: "./",
 
-    files: [
-      "app/bower_components/angular/angular.js",
-      "app/bower_components/angular-route/angular-route.js",
-      "app/bower_components/angular-mocks/angular-mocks.js",
-      "app/bower_components/underscore/underscore.js"
-    ],
+        files: [
+            "app/bower_components/angular/angular.js",
+            "app/bower_components/angular-route/angular-route.js",
+            "app/bower_components/angular-mocks/angular-mocks.js",
+            "app/bower_components/underscore/underscore.js"
+        ],
 
-    proxies: {
-      "/node_modules": "/base/node_modules",
-      "/base/cards": "/base/app/cards"
-    },
+        proxies: {
+            "/node_modules": "/base/node_modules",
+            "/base/cards": "/base/app/cards"
+        },
 
-    jspm: {
-      config: "app/config.js",
-      packages: "app/bower_components/system.js/dist",
-      serveFiles: [
-        "app/**/*.js",
-        "node_modules/babel-core/browser.js"
-      ],
-      loadFiles: [
-        "tests/cards/*.js",
-        "tests/klondike/**/*.js"
-      ]
-    },
+        jspm: {
+            config: "app/config.js",
+            packages: "app/bower_components/system.js/dist",
+            serveFiles: [
+                "app/**/*.js",
+                "node_modules/babel-core/browser.js"
+            ],
+            loadFiles: [
+                "tests/cards/*.js",
+                "tests/klondike/**/*.js"
+            ]
+        },
 
-    autoWatch: true,
+        autoWatch: true,
 
-    frameworks: ["jspm", "jasmine"],
+        frameworks: ["jspm", "jasmine"],
 
-    browsers: ["Chrome"],
+        browsers: ["Chrome"],
 
-    plugins: [
-      "karma-jspm",
-      "karma-chrome-launcher",
-      "karma-jasmine",
-      "karma-junit-reporter"
-    ],
+        plugins: [
+            "karma-jspm",
+            'karma-phantomjs-launcher',
+            "karma-chrome-launcher",
+            "karma-jasmine",
+            "karma-junit-reporter"
+        ],
 
-    junitReporter: {
-      outputFile: "test_out/unit.xml",
-      suite: "unit"
-    }
+        junitReporter: {
+            outputFile: "test_out/unit.xml",
+            suite: "unit"
+        }
 
-  });
+    });
 };
